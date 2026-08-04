0f551fef26b29c11bacbef81d1167911c10c507f
TepidmishA <perep.main@mail.ru>
2026-08-04 17:08:53 +0300

    fix paths

--- configs/gaucho/dotav15/gaucho_anchorless_dotav1_5/gaussian_fcos_r50_fpn_gaucho_gwd_1x_dota15_le90.py
+++ configs/gaucho/dotav15/gaucho_anchorless_dotav1_5/gaussian_fcos_r50_fpn_gaucho_gwd_1x_dota15_le90.py
@@ -1,6 +1,6 @@
 _base_ = [
-    '../_base_/datasets/dotav15.py', '../_base_/schedules/schedule_1x.py',
-    '../_base_/default_runtime.py'
+    '../../../_base_/datasets/dotav15.py', '../../../_base_/schedules/schedule_1x.py',
+    '../../../_base_/default_runtime.py'
 ]
 
 ################################################

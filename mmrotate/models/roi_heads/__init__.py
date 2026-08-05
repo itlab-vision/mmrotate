d55f8b23d6c01a3613fc9d963132971e11d5dc00
TepidmishA <perep.main@mail.ru>
2026-08-05 15:23:20 +0300

    add head

--- mmrotate/models/roi_heads/__init__.py
+++ mmrotate/models/roi_heads/__init__.py
@@ -6,9 +6,10 @@ from .oriented_standard_roi_head import OrientedStandardRoIHead
 from .roi_extractors import RotatedSingleRoIExtractor
 from .roi_trans_roi_head import RoITransRoIHead
 from .rotate_standard_roi_head import RotatedStandardRoIHead
+from .faa_head import FAAHead
 
 __all__ = [
     'RotatedBBoxHead', 'RotatedConvFCBBoxHead', 'RotatedShared2FCBBoxHead',
     'RotatedStandardRoIHead', 'RotatedSingleRoIExtractor',
-    'OrientedStandardRoIHead', 'RoITransRoIHead', 'GVRatioRoIHead'
+    'OrientedStandardRoIHead', 'RoITransRoIHead', 'GVRatioRoIHead', 'FAAHead'
 ]

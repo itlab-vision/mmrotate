599d67dc981692569123168a7c99f7f8e72870f0
TepidmishA <perep.main@mail.ru>
2026-08-14 10:17:49 +0300

    try fix mAP=0 `roi_transformer_r50_fpn_gaucho_probiou_1x_dota_ms_le90.py`

--- configs/gaucho/dotav1/SOTA/roi_transformer_r50_fpn_gaucho_probiou_1x_dota_ms_le90.py
+++ configs/gaucho/dotav1/SOTA/roi_transformer_r50_fpn_gaucho_probiou_1x_dota_ms_le90.py
@@ -124,7 +124,8 @@ model = dict(
                 roi_feat_size=7,
                 num_classes=num_classes,
                 bbox_coder=dict(
-                    type='DeltaXYWHAHBBoxCoder',
+                    type=coder,
+                    horizontal=True,
                     angle_range=angle_version,
                     norm_factor=2,
                     edge_swap=True,
@@ -135,8 +136,9 @@ model = dict(
                     type='CrossEntropyLoss',
                     use_sigmoid=False,
                     loss_weight=1.0),
-                loss_bbox=dict(type='SmoothL1Loss', beta=1.0,
-                               loss_weight=1.0)),
+                gaucho_encoding=use_gaucho,
+                reg_decoded_bbox=reg_decoded_bbox,
+                loss_bbox=gaussian_loss),
             dict(
                 type='RotatedShared2FCBBoxHead',
                 in_channels=256,

6cb31ad7dd223383d6195ac61f53af5df2c5bb2b
TepidmishA <perep.main@mail.ru>
2026-09-03 16:46:58 +0300

    optimize gaucho configs

--- configs/gaucho/dotav15/SOTA/gaussian_fcos_r50_fpn_gaucho_probiou_1x_dota15_ms_rr_le90.py
+++ configs/gaucho/dotav15/SOTA/gaussian_fcos_r50_fpn_gaucho_probiou_1x_dota15_ms_rr_le90.py
@@ -1,7 +1,6 @@
 _base_ = [
-    '../../../_base_/datasets/dotav15_ms.py',
-    '../../../_base_/schedules/schedule_1x.py',
-    '../../../_base_/default_runtime.py'
+    '../gaucho_anchorless_dotav1_5/'
+    'gaussian_fcos_r50_fpn_gaucho_probiou_1x_dota15_le90.py'
 ]
 
 ################################################
@@ -9,31 +8,10 @@ _base_ = [
 
 angle_version = 'le90'
 
-num_classes = 16
-
-use_gaucho = True
-
-gaussian_centerness = False
-
-norm_on_bbox = False
-
-coder = 'GauchoAnchorOBBDecoder'
-
-reg_decoded_bbox = True
-
-stds = [1.0, 1.0, 1.0, 1.0, 1.0]
-
-gaussian_loss = dict(
-    type='GDLoss_v1',
-    gaussian_prediction=True,
-    loss_type='probiou',
-    fun='log1p',
-    tau=1.0,
-    loss_weight=1.0)
-
+dataset_type = 'DOTAv15Dataset'
+data_root = 'data/split_ms_dota_1_5/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
-
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
@@ -57,66 +35,19 @@ train_pipeline = [
 ]
 
 data = dict(
-    train=dict(pipeline=train_pipeline, version=angle_version),
-    val=dict(version=angle_version),
-    test=dict(version=angle_version))
-
-################################################
-################################################
-
-# model settings
-model = dict(
-    type='RotatedFCOS',
-    backbone=dict(
-        type='ResNet',
-        depth=50,
-        num_stages=4,
-        out_indices=(0, 1, 2, 3),
-        frozen_stages=1,
-        zero_init_residual=False,
-        norm_cfg=dict(type='BN', requires_grad=True),
-        norm_eval=True,
-        style='pytorch',
-        init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet50')),
-    neck=dict(
-        type='FPN',
-        in_channels=[256, 512, 1024, 2048],
-        out_channels=256,
-        start_level=1,
-        add_extra_convs='on_output',  # use P5
-        num_outs=5,
-        relu_before_extra_convs=True),
-    bbox_head=dict(
-        type='GaussianFCOSHead',
-        num_classes=num_classes,
-        in_channels=256,
-        stacked_convs=4,
-        feat_channels=256,
-        strides=[8, 16, 32, 64, 128],
-        center_sampling=True,
-        center_sample_radius=1.5,
-        norm_on_bbox=norm_on_bbox,
-        centerness_on_reg=True,
-        separate_angle=False,
-        scale_angle=True,
-        bbox_coder=dict(
-            type='DistanceAnglePointCoder', angle_version=angle_version),
-        loss_cls=dict(
-            type='FocalLoss',
-            use_sigmoid=True,
-            gamma=2.0,
-            alpha=0.25,
-            loss_weight=1.0),
-        gaucho_encoding=use_gaucho,
-        gaussian_centerness=gaussian_centerness,
-        loss_bbox=gaussian_loss,
-        loss_centerness=dict(
-            type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0)),
-    # training and testing settings
-    train_cfg=None,
-    test_cfg=dict(
-        nms_pre=2000,
-        min_bbox_size=0,
-        score_thr=0.05,
-        nms=dict(iou_thr=0.1),
-        max_per_img=2000))
+    train=dict(
+        type=dataset_type,
+        ann_file=data_root + 'trainval/annfiles/',
+        img_prefix=data_root + 'trainval/images/',
+        pipeline=train_pipeline,
+        version=angle_version),
+    val=dict(
+        type=dataset_type,
+        ann_file=data_root + 'trainval/annfiles/',
+        img_prefix=data_root + 'trainval/images/',
+        version=angle_version),
+    test=dict(
+        type=dataset_type,
+        ann_file=data_root + 'test/images/',
+        img_prefix=data_root + 'test/images/',
+        version=angle_version))

2de47853208aa1bdc62409a66b662c1e58b53961
TepidmishA <perep.main@mail.ru>
2026-09-09 16:25:54 +0300

    change cfa evaluation step back from 5 to 40

--- configs/cfa/cfa_r50_fpn_40e_dota_oc.py
+++ configs/cfa/cfa_r50_fpn_40e_dota_oc.py
@@ -1,7 +1,7 @@
 _base_ = ['./cfa_r50_fpn_1x_dota_oc.py']
 
 # evaluation
-evaluation = dict(interval=5, metric='mAP')
+evaluation = dict(interval=40, metric='mAP')
 # learning policy
 lr_config = dict(step=[24, 32, 38])
 runner = dict(type='EpochBasedRunner', max_epochs=40)
